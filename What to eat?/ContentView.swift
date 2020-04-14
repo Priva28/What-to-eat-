62de42d3770b89c112786708ed8f42edf593e73b
Christian <priva2804@gmail.com>
2020-04-14 18:30:19 +1000

    Initial Commit

---
+++ What to eat?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  What to eat?
+//
+//  Created by Christian Privitelli on 14/4/20.
+//  Copyright © 2020 Christian Privitelli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
